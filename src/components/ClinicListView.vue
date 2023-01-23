13c1e7f1a9ebee52089b5a0f52feec5b5ec92bc1
Aniket Sawant <sawantaniket622@gmail.com>
2023-01-23 17:02:10 +0530

    coment on jan 23

---
+++ src/components/ClinicListView.vue
@@ -0,0 +1,115 @@
+<template> 
+   <home-view></home-view>
+     <body id="app">
+        <table class="table">
+            <thead>
+               <tr>
+                <th>Clinic ID</th>                
+                <th>Clinic Name</th>
+                <th>Clinic Address</th>
+                <th>Clinic City</th>
+                <th>State</th>
+                <th>Pincode</th>
+                <th>Date Added</th>
+                <th>Status</th>
+                <th>Last Modified By</th>
+                <th>Action</th>
+               </tr>
+            </thead>
+            <tbody>
+                <tr v-for="clinicList in list" :key="clinicList">
+                    <td > {{(clinicList.clinicId)}}</td>
+                    <td>{{(clinicList.clinicName)}}</td>
+                    <td>{{(clinicList.clinicAddress)}}</td>
+                    <td>{{(clinicList.clinicCity)}}</td>
+                    <td>{{(clinicList.clinicState)}}</td>                   
+                    <td>{{(clinicList.clinicPincode)}}</td>
+                    <td>{{(clinicList.dateAdded)}}</td>
+                    <td>{{(clinicList.status)}}</td>
+                    <td>{{(clinicList.lastModifedBy)}}</td>
+                    <td>
+                        <router-link class="router-link"  to="DoctorRegisterView"><i class="fa-solid fa-pencil"></i></router-link>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+       
+    </body>
+</template>
+<script>
+import axios from 'axios';
+export default{
+    name: "ClinicListView",
+    data(){
+        return{
+            list:[]
+        }
+    },
+    async mounted(){
+        axios.get('http://localhost:8080/clinicList',{})
+        .then(response => {
+            this.list = response.data;
+            console.log(response.data);
+        })
+        .catch(error =>{
+            this.errors.push(error);
+            console.log(error);
+        })
+    },
+
+    
+}
+
+</script>
+
+<style scoped>
+#app{
+    width: 100%;
+}
+.table{
+    width: 100%;
+    font-family: arial, sans-serif;
+  font-size: 12px;
+  border-collapse: collapse;
+  width: 100%;
+  text-align: center;
+}
+.router-link{
+    text-decoration: none;
+    background-color: rgb(10, 116, 255);
+    color: rgb(255, 255, 255);
+    font-size: 16px;
+    margin: 5px;
+    padding: 5px;
+    box-sizing: border-box;
+}
+.router-link:hover{
+    position: relative;
+    top: 2px;
+    background-color: rgb(65, 147, 255);
+}
+
+ 
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+  text-align: center;
+}
+
+thead{
+    background-color: rgb(26, 25, 25);
+    color: white;
+    border: rgb(124, 124, 124) 1px solid;
+  
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+tbody td,tr{
+    border: rgb(124, 124, 124) 1px solid;
+}
+
+</style>
---
+++ src/components/LoginView.vue
@@ -0,0 +1,112 @@
+<template>
+    <body>
+        <img src="../assets/logo.png" alt="" style="position: relative;">
+        <div class="form">
+            <img src="../assets/login.png" alt="" style="margin-left: 22%;">
+            <br><br>
+            <div class="input-group mb-3">
+             <span class="input-group-text" id="basic-addon1">@</span>
+             <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" v-model="username" name="username">
+            </div><br>
+            <div class="input-group flex-nowrap">
+                <span class="input-group-text" id="addon-wrapping">#</span>
+                <input type="text" class="form-control" placeholder="Password" aria-label="Username" aria-describedby="addon-wrapping" v-model="password" name="password">
+            </div>
+            <small style="color: red;">{{ message }}</small><br><br>
+            <div class="d-grid gap-2">
+                <button class="login" type="button" @click="login(username, password)"><b>LogIn</b></button>
+            </div>
+            <br><br><br>
+            <hr style="color: white; height:1px;">
+            <div class="foot">
+              Don't Have an Account? Please 
+              <router-link class="router-link"  to="SignUp"> Register</router-link>
+
+              <router-link class="router-link"  to="image"> Register</router-link>              
+          </div>
+        </div>
+
+  
+    </body>
+</template>
+<script>
+    export default{
+        data(){
+          return{
+              message:''
+          }
+      },
+        methods:{
+            login(username, password){
+                if (username === 'admin' && password === 'admin') {
+                    this.$router.push({path:`/HomeView`})
+                }
+                else{
+                    this.message = "Username and Password incorrect"
+                }
+            }
+        }
+    }
+</script>
+<style scoped>
+body {
+    background-image: url("../assets/AdminBackground.jpg");
+    background-size: 100%;
+    background-repeat: repeat;
+    font-family: 'Roboto', sans-serif;
+    height: 100vh;
+    position: relative;
+    padding: 30px;
+    overflow-y: hidden;
+    /* Hide vertical scrollbar */
+    overflow-x: hidden;
+    /* Hide horizontal scrollbar */
+}
+
+.form {
+    box-sizing: border-box;
+    width: 600px;
+    height: 450px;
+    margin: auto;
+    margin-top: 50px;
+    background-color: #00000080;
+    padding: 50px;
+    color: white;
+    border-radius: 30px;
+    /* box-shadow: 0px 0px 27px rgb(109, 109, 109); */
+}
+
+.login{
+    background-color:  #ffeba7;
+    border: 1px #f8dd82 solid;
+    border-radius: 7px;
+    height: 40px;
+    color: rgb(66, 66, 66);
+}
+
+.login:hover{
+    background-color:  #fce28c;
+    box-shadow: #f8dd82 1px 1px 15px;
+    font-size: 16px;
+}
+.form-control{
+    background: transparent;
+    color: white;
+    border-color: white;
+}
+.input-group-text{
+    background: transparent;
+    border-color: white;
+}
+
+.foot{
+    text-align: center;
+    font-size: 12px ;
+   
+}
+.router-link{
+    text-decoration: none;
+    color: rgb(33, 122, 255);
+
+}
+</style>
